48abb5ea89f9a8ddcb21881e824d3abbe8e2d7b6
http://jneen.net/ <jneen@jneen.net>
2020-12-11 11:37:49 -0500

    make `..` act like a real operator, allowing x..y ranges

--- lib/dentaku/ast/range.rb
+++ lib/dentaku/ast/range.rb
@@ -1,10 +1,31 @@
-require_relative "./literal"
+require_relative "./operation"
 
 module Dentaku
   module AST
-    class Range < Literal
-      def value_type
-        :range
+    class Range < Operation
+      def types
+        [:numeric, :numeric, :range]
+      end
+
+      # must return something responding to ===, in this case a
+      # regular ruby Range.
+      def value
+        (left.value..right.value)
+      rescue
+        binding.pry
+      end
+
+      def generate_constraints!(context)
+        # pass. this is handled by AST::Case
+      end
+
+      # higher than comparators (5) lower than any kind of math (10+)
+      def self.precedence
+        8
+      end
+
+      def repr
+        "#{left.repr}..#{right.repr}"
       end
     end
   end
