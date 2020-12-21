37c74bbe6a2ff74373043121d1258a8ce6881312
http://jneen.net/ <jneen@jneen.net>
2020-12-21 14:53:36 -0500

    add a comment about `== false`

--- lib/dentaku/ast/combinators.rb
+++ lib/dentaku/ast/combinators.rb
@@ -16,6 +16,8 @@ module Dentaku
         # short-circuit from both sides of AND and OR expressions, so that we
         # don't demand data from users that is not necessary to evaluate the
         # expression logically.
+        #
+        # NOTE: need `== false` here because the result is true/false/nil
         return false if left.partial_evaluate == false
         return false if right.partial_evaluate == false
 
