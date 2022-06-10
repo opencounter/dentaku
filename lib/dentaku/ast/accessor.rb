c0295d0855d2bc02e09cfe5b0141b870b6167275
http://jneen.net/ <jneen@jneen.net>
2022-06-10 18:34:44 -0400

    add accessor ast node

---
+++ lib/dentaku/ast/accessor.rb
@@ -0,0 +1,26 @@
+module Dentaku
+  module AST
+    class Accessor < Node
+      def initialize(expr, accessor)
+        @expr = expr
+        @accessor = accessor
+      end
+
+      def children
+        [@expr]
+      end
+
+      def generate_constraints(context)
+        @expr.generate_constraints(context)
+        lookup = Type::Expression.key_of(Type::Expression.syntax(@expr), @accessor)
+        context.add_constraint!([:syntax, self],
+                                lookup,
+                                [:accessor, @expr, @accessor])
+      end
+
+      def repr
+        "(#{@expr.repr}.#{@accessor})"
+      end
+    end
+  end
+end
